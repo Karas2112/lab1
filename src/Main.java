6784ff053b293b4ceb2cf838a6191788d50da6fe
Bartosz Karaś <bkaras2112@gmail.com>
2021-10-02 10:59:40 +0200

    zadanie 5 i 6 nadal

--- src/Main.java
+++ src/Main.java
@@ -39,9 +39,15 @@ public class Main {
         me.lastName = "Karaś";
         me.pet = cat;
         me.setSalary(5000.0);
+
         System.out.println("I have a " + me.pet.species);
         System.out.println("I call it  " + me.pet.name);
         me.setCar(pierdzikółko);
+
+        if(me.getCar()!=null){
+            System.out.println(me.getCar().model);
+        }
+
         System.out.println("I have a car, which is " + me.getCar().producer + " " + me.getCar().model);
         System.out.println("It has " + me.getCar().doorsQuantity + " doors and I was driving it for the last " + me.getCar().mileage + "km");
         System.out.println(me.pet.weight);
